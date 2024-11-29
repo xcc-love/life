33b3557fec61388c8b8af06f931af2a1269b9025
xcc <867747941@qq.com>
2024-11-29 14:11:02 +0800

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,6 @@ cd public
 
 # 初始化 Git 仓库
 echo "Initializing a new Git repository..."
-rm -rf .git  # 清理已有的 Git 仓库（可选）
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -23,7 +22,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.gitee.io/<REPO>
 echo "Pushing to GitHub..."
-git push -f git@github.com:xcc-love/life.git main:page
+git push -f git@github.com:xcc-love/life.git master:pages
 
 echo "Deployment complete!"
 cd -
