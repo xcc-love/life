e330d8e061988da3ca70206accde346ec685a9eb
xcc <867747941@qq.com>
2024-11-29 11:23:43 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,10 @@ npm run build
 # 进入生成的文件夹
 cd public
 
+# 初始化 Git 仓库
+echo "Initializing a new Git repository..."
+rm -rf .git  # 清理已有的 Git 仓库（可选）
+
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
@@ -17,10 +21,9 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.gitee.io
-# git push -f git@gitee.com:<USERNAME>/<USERNAME>.gitee.io.git master
-
 # 如果发布到 https://<USERNAME>.gitee.io/<REPO>
-git push -f git@github.com:xcc-love/life.git main:pages
+echo "Pushing to GitHub..."
+git push -f git@github.com:xcc-love/life.git main:page
 
+echo "Deployment complete!"
 cd -
